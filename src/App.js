d9014e4fdfd5b33be5fdb5242a64e1f534b849ea
Bar Horing <bar.apple@gmail.com>
2019-04-13 13:37:01 +0300

    new component Pet rendered inside App component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ const Pet = () => {
     React.createElement("h2", {}, "Dog"),
     React.createElement("h2", {}, "Havanese")
   ]);
-}
+};
 
 const App = () => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me"),
+    React.createElement("h1", {}, "Adopt Me now!"),
     React.createElement(Pet),
     React.createElement(Pet),
     React.createElement(Pet)
   ]);
 };
-ReactDOM.render(React.createElement(App), document.getElementById("root"), () => console.log("Bar Horing Amir"));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
