b1a263465f2abf04ec246359112a5a7424338c20
Bar Horing <bar.apple@gmail.com>
2019-04-21 22:04:34 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
+// at the top
+import pf from "petfinder-client";
+
+// under imports
+// Note (April 2nd, 2019): You no longer need real API keys, Petfinder Client will still work
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class App extends React.Component {
+  // inside class, above render
+  componentDidMount() {
+    petfinder.breed.list({ animal: "bird" }).then(console.log, console.error);
+  }
   render() {
     return (
       <div>
