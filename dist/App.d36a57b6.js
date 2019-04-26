2593bef4391b4d5c758e60a28454165e16b36499
Bar Horing <bar.apple@gmail.com>
2019-04-26 17:41:07 +0300

    remove constractor and super with babel. Specifically this was done with transform-class-properties

--- dist/App.d36a57b6.js
+++ dist/App.d36a57b6.js
@@ -49388,16 +49388,22 @@ var Details =
 function (_React$Component) {
   _inherits(Details, _React$Component);
 
-  function Details(props) {
+  function Details() {
+    var _getPrototypeOf2;
+
     var _this;
+
+    var _temp;
 
     _classCallCheck(this, Details);
 
-    _this = _possibleConstructorReturn(this, _getPrototypeOf(Details).call(this, props));
-    _this.state = {
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    return _possibleConstructorReturn(_this, (_temp = _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(Details)).call.apply(_getPrototypeOf2, [this].concat(args))), _this.state = {
       loading: true
-    };
-    return _this;
+    }, _temp));
   }
 
   _createClass(Details, [{
