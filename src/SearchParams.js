f752639001a0d7d4d61ed72a952b51d18f82c149
Bar Horing <bar.apple@gmail.com>
2019-05-11 10:34:33 +0300

    using context. step 1

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,54 +1,7 @@
 import React from "react";
-import pf, { ANIMALS } from "petfinder-client";
-
-const petfinder = pf({
-  key: process.env.API_KEY,
-  secret: process.env.API_SECRET
-});
+import { ANIMALS } from "petfinder-client";
 
 class SearchParams extends React.Component {
-  state = {
-    location: "Seattle, WA",
-    animal: "",
-    breed: "",
-    breeds: []
-  };
-
-  handleLocationChange = event => {
-    this.setState({ location: event.target.value });
-  };
-
-  handleAnimalChange = event => {
-    this.setState(
-      {
-        animal: event.target.value,
-        breed: ""
-      },
-      this.getBreeds
-    );
-  };
-
-  handleBreedChange = event => {
-    this.setState({ breed: event.target.value });
-  };
-
-  getBreeds() {
-    if (this.state.animal) {
-      petfinder.breed.list({ animal: this.state.animal }).then(data => {
-        console.log(data);
-        if (
-          data.petfinder &&
-          data.petfinder.breeds &&
-          Array.isArray(data.petfinder.breeds.breed)
-        )
-          this.setState({ breeds: data.petfinder.breeds.breed });
-        else {
-          this.setState({ breeds: [] });
-        }
-      });
-    }
-  }
-
   render() {
     return (
       <div className="search-params">
